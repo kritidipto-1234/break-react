35efb94445f30030b370dcbbfb9c7b8512076f8f
Kritidipto Ghosh <kritidipto.ghosh@wingify.com>
2024-11-26 00:26:37 +0530

    feat(): buton text

---
+++ src/Order.tsx
@@ -0,0 +1,17 @@
+const Order = () => {
+    return <div>
+        {/* <div id="container1"> */}
+            <div id="one">one</div>
+            <div id="two">two</div>
+            <div id="three">three</div>
+        {/* </div> */}
+        {/* <div id="container2">    */}
+            <div id="four">four</div>
+            <div id="five">five</div>
+            <div id="six">six</div>
+        {/* </div> */}
+
+    </div>;
+}
+
+export default Order;
